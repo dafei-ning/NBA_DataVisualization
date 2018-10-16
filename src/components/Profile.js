9f668b2b15bc393a32d540ad254d12f7417438d3
Dafei Ning <ningdafei@gmail.com>
2018-10-15 21:41:50 -0700

    update profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {PROFILE_PIC_URL_PREFIX, TEAM_PIC_URL_PREFIX} from '../constants.js';
+//import * as constants from '../constants.js';
 
 export class Profile extends React.Component {
 
@@ -21,7 +23,60 @@ export class Profile extends React.Component {
 		} = this.props.playerInfo;
 
 		return (
-			<div className="profile">Profile</div>
+			<div className="profile">
+			<div className="profile-entry player-name">{`${playerName}`}</div>
+
+			<img
+				className="profile-pic"
+				src={`${PROFILE_PIC_URL_PREFIX}/${playerId}.png`}
+				alt="Profile"
+
+			/>
+
+
+			<div className="profile-entry">			
+				<div className="profile-entry-left">Team</div>
+				<div className="profile-entry-right">{`${teamCity} ${teamName}`}</div>
+			</div>
+			<img
+                className="team-logo"
+                src={`${TEAM_PIC_URL_PREFIX}/${teamAbbreviation}_logo.svg`}
+                alt="Team"
+            />
+
+			<div className="profile-entry">			
+				<div className="profile-entry-left">Height</div>
+				<div className="profile-entry-right">{`${height}`}</div>
+			</div>
+
+			<div className="profile-entry">			
+				<div className="profile-entry-left">Weight</div>
+				<div className="profile-entry-right">{`${weight}`}</div>
+			</div>
+
+			<div className="profile-entry">			
+				<div className="profile-entry-left">Pts</div>
+				<div className="profile-entry-right">{`${pts}`}</div>
+			</div>
+
+			<div className="profile-entry">			
+				<div className="profile-entry-left">Reb</div>
+				<div className="profile-entry-right">{`${reb}`}</div>
+			</div>
+
+			<div className="profile-entry">			
+				<div className="profile-entry-left">Ast</div>
+				<div className="profile-entry-right">{`${ast}`}</div>
+			</div>
+
+			<div className="profile-entry">			
+				<div className="profile-entry-left">Pie</div>
+				<div className="profile-entry-right">{`${pie}`}</div>
+			</div>
+
+
+
+			</div>
 		);
 	}
 
