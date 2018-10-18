8e3f2f5f51659dcebd3ae4192679d3a039180b04
Dafei Ning <ningdafei@gmail.com>
2018-10-17 19:50:59 -0700

    Add countslider to radioButton to DataView before design

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Radio } from 'antd';
 import { ShotChart } from './ShotChart';
 import { CountSlider } from './CountSlider';
+
+const RadioGroup = Radio.Group;
 
 export class DataViewContainer extends React.Component {
     render() {
@@ -13,6 +16,11 @@ export class DataViewContainer extends React.Component {
                     displayToolTips={true}
                 />
                 <CountSlider />
+                <RadioGroup onChange={this.onChange} value={1}>
+                    <Radio value={1}>Hexbin</Radio>
+                    <Radio value={2}>Scatter</Radio>
+                  
+                </RadioGroup>
 
             </div>
         );
